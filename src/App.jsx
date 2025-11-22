3f6be32218a72b0e454ec71cc68de69e8ee08dc3
Kemal Atayev <kemalatayew913@gmail.com>
2025-11-22 23:54:09 +0500

    2.30

--- src/App.jsx
+++ src/App.jsx
@@ -525,7 +525,7 @@ const InteractiveFeaturesSection = () => {
   );
 };
 
-// Секци
+// Секц
 const StatsSection = () => {
   const containerRef = useRef(null);
   const { scrollYProgress } = useScroll({
