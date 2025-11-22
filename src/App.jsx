20d93ea9472181d60e4084faa8c69afd492e951f
Kemal Atayev <kemalatayew913@gmail.com>
2025-11-22 21:21:02 +0500

    1.62

--- src/App.jsx
+++ src/App.jsx
@@ -87,7 +87,7 @@ const HorizontalScrollSection = () => {
       title: "ОЗУ 'Вектор'",
       description: "Высокочастотная память для максимальной производительности.",
       color: "#10b981",
-      image: "/images/ram.png" // Пример пут
+      image: "/images/ram.png" // Пример пу
     },
     {
       title: "Корпус 'Цитадель'",
