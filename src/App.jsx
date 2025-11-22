dcec9ecf2ecbf12f9857e0a4ea95ad4bfa61fb52
Kemal Atayev <kemalatayew913@gmail.com>
2025-11-22 23:40:41 +0500

    2.9

--- src/App.jsx
+++ src/App.jsx
@@ -853,7 +853,7 @@ function App() {
             transition={{ delay: 0.3 }}
           >
             <div className="contact-info">
-              <p>📧 your.email@exa</p>
+              <p>📧 your.email@ex</p>
               <p>📱 +993 (64) 00 - 53 - 74</p>
               <p>📍 Ашхабад, Туркменистан</p>
             </div>
