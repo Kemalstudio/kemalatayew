7a6763c304cfa003af81124a7e0f2fd4ff16c7c9
Kemal Atayev <kemalatayew913@gmail.com>
2025-11-23 21:52:08 +0500

    3.0

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ const Crazy3DImageSlider = () => {
   return (
     <section className="crazy-3d-slider-section">
       <div className="container">
-        {/* Заголовок секции остался без из */}
+        {/* Заголовок секции остался без и */}
         <motion.div
           className="section-header"
           initial={{ opacity: 0, y: 50 }}
