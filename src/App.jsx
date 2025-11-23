2b54fb4336f8e62015096d007b47b4b9b5b06fbf
Kemal Atayev <kemalatayew913@gmail.com>
2025-11-23 21:33:29 +0500

    2.82

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ const Crazy3DImageSlider = () => {
     { src: "/images/atam.jpg" },
     { src: "/images/atam.jpg" },
     { src: "/images/atam.jpg" },
-    { src: "/images/atam.jpg" }, // Добавим больш
+    { src: "/images/atam.jpg" }, // Добавим боль
     { src: "/images/atam.jpg" },
   ];
 
