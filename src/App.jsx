0a1119a4162f4ba5dedf59b471aad127bd3043b4
Kemal Atayev <kemalatayew913@gmail.com>
2025-11-24 23:41:32 +0500

    3.96

--- src/App.jsx
+++ src/App.jsx
@@ -174,7 +174,7 @@ const Crazy3DImageSlider = () => {
     if (sceneRef.current?.timeline) {
       gsap.to(sceneRef.current.timeline, { timeScale: 1, duration: 0.5 });
     }
-    // Возврат в исходное состояние с провер
+    // Возврат в исходное состояние с прове
     if (sceneRef.current) {
       gsap.to(sceneRef.current, {
         rotationX: 0,
