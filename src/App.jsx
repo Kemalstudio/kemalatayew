0368d747015dd77dd8dea022fbc4e7f81d701530
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-02 22:11:04 +0500

    7.49

--- src/App.jsx
+++ src/App.jsx
@@ -554,7 +554,7 @@ function App() {
         <SkillsSection />
         <StatsSection />
         
-        {/* ВОТ ЗДЕСЬ СЕКЦИЯ КОТОРА  */}
+        {/* ВОТ ЗДЕСЬ СЕКЦИЯ КОТОР  */}
         <Crazy3DImageSlider />
         
         <section className="services-grid">
