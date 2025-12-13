8591802e8ae0042634b837d7e09d0abd48d45035
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-13 12:39:41 +0500

    8.30

--- src/App.jsx
+++ src/App.jsx
@@ -591,7 +591,7 @@ function App() {
         <section className="hero">
           <SmoothParallaxStars />
           <div className="hero-content">
-            {/ 
+            {/
             <ModelViewer modelPath="/images/gaming_desktop_pc.glb" />
           </div>
           <div className="ticker-section">
