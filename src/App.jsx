b6c81d8c3f15e1f984e2670c03ea7b260da9428a
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-14 23:16:46 +0500

    10.64

--- src/App.jsx
+++ src/App.jsx
@@ -819,7 +819,7 @@ function App() {
         {/* Секция с карточками */}
         <OverviewSection />
         
-        {/* НОВАЯ СЕКЦИЯ С 3D ШАРАМ
+        {/* НОВАЯ СЕКЦИЯ С 3D ШАРА
         <TechBallSection />
         
         <SkillsSection />
