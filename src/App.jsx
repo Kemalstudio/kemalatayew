e194c46532058be338abaa8abe270a2e1035c098
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-20 20:48:22 +0500

    11.53

--- src/App.jsx
+++ src/App.jsx
@@ -335,7 +335,7 @@ useGLTF.preload('/images/gaming-desktop.glb');
 // Компонент одного шара
 const Ball = (props) => {
   // Загружаем текстуру. Если URL неверен, может быть ошибка, поэтому 
-  // используйте существующие картинки ил
+  // используйте существующие картинки и
   const [decal] = useTexture([props.imgUrl]);
 
   return (
