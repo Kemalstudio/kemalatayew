ee118bcba7763b4bead6ba0b24d6229e05fe74bf
Kemal Atayev <kemalatayew913@gmail.com>
2025-12-31 13:13:52 +0500

    12.18

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import {
   Environment, 
   ContactShadows, 
   useProgress,
-  Decal,     // Добавл
+  Decal,     // Добав
   Preload,   // Добавлено для шаров
   useTexture // Добавлено для шаров
 } from '@react-three/drei';
