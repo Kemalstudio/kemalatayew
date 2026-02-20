c24a0b769ad7fed856af0c98a524ee983bd5e8d1
Kemal Atayev <kemalatayew913@gmail.com>
2026-02-20 11:46:13 +0500

    12.62

--- src/App.jsx
+++ src/App.jsx
@@ -175,7 +175,7 @@ const GsapPanelsShowcase = () => {
           trigger: containerRef.current,
           pin: true,
           scrub: 1,
-          end: () => "+=" + window.innerHeight * 4, // 4 экрана скролла
+          end: () => "+=" + window.innerHeight * 4, // 4 экрана скролл
         }
       });
 
