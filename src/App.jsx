dc88b4593e3ae99309e3b08cb3f2936eb47fa88f
Kemal Atayev <kemalatayew913@gmail.com>
2026-02-24 00:07:37 +0500

    13.95

--- src/App.jsx
+++ src/App.jsx
@@ -430,7 +430,7 @@ const TechBentoGrid = ({ dict, techStack }) => {
         ease: "power4.out"
       });
 
-      // Анима
+      // Аним
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: sectionRef.current,
