b80fbc76461c213eabb4d28cbb43bd46f7f6cd63
Kemal Atayev <kemalatayew913@gmail.com>
2026-02-23 23:11:28 +0500

    13.62

--- src/App.jsx
+++ src/App.jsx
@@ -322,7 +322,7 @@ const TechBentoGrid = ({ dict, techStack }) => {
 // ==========================================
 export default function App() {
   const [lang, setLang] = useState('en');
-  const [a11yMode, setA11yMode] = useState(false); // Состояние Режима Доступн
+  const [a11yMode, setA11yMode] = useState(false); // Состояние Режима Доступ
   const dict = translations[lang];
 
   const techStack = [
