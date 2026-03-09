68721c6d2389a57d20fa39f000c7636dd45956d6
Kemal Atayev <kemalatayew913@gmail.com>
2026-03-09 23:53:09 +0500

    17.0

--- src/App.jsx
+++ src/App.jsx
@@ -124,7 +124,7 @@ const CustomCursor = () => {
       xToFollower(e.clientX);
       yToFollower(e.clientY);
 
-      // Логика обнаружения дата-атрибута для смены состояния ку
+      // Логика обнаружения дата-атрибута для смены состояния к
       const target = e.target.closest('[data-cursor]');
       if (target) {
         setIsHovered(true);
